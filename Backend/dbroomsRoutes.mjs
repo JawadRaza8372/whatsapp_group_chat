5be6291b1fe4cf1581aee7d235f46a94ee5c9e10
Jawad Raza <jawadraza8372@gmail.com>
2021-10-20 14:39:43 +0500

    some changes

--- Backend/dbroomsRoutes.mjs
+++ Backend/dbroomsRoutes.mjs
@@ -1,29 +1,27 @@
 import express from "express";
 import roomSchema from "./dbrooms.mjs";
-const Router=express();
+const Router = express();
 
-Router.get("/room/sync",(req,res,next)=>{
-    roomSchema.find((err,data)=>{
-        if (err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(200).send(data)
-        }
+Router.get("/room/sync", (req, res, next) => {
+  roomSchema
+    .find()
+    .then((data) => {
+      res.send(data);
     })
+    .catch((e) => {
+      res.send(e);
+    });
 });
 
-Router.post("/room/new",(req,res,next)=>{
-    const dbmessage=req.body;
-    console.log(req.body)
-    roomSchema.create(dbmessage,(err,data)=>{
-        if (err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(201).send(data)
-        }
-    })
-   
-})
+Router.post("/room/new", (req, res, next) => {
+  const dbmessage = req.body;
+  console.log(req.body);
+  roomSchema.create(dbmessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 export default Router;
--- Client/src/App.js
+++ Client/src/App.js
@@ -1,59 +1,69 @@
-import './App.css';
-import {useEffect,useState} from "react"
-import Pusher from "pusher-js"
-import axios from "axios"
-import {fetchmessage,fetchroom} from "./Apis/Api"
-import Routes from './Navigation/Routes';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import axios from "axios";
+import { fetchmessage, fetchroom } from "./Apis/Api";
+import Routes from "./Navigation/Routes";
 function App() {
-const [messages, setmessages] = useState([])
-const [myroomData, setmyroomData] = useState([])
-const [userData, setuserData] = useState(null)
-const [fetchroomd, setfetchroomd] = useState(false)
+  const [messages, setmessages] = useState([]);
+  const [myroomData, setmyroomData] = useState([]);
+  const [userData, setuserData] = useState(null);
+  const [fetchroomd, setfetchroomd] = useState(false);
 
-
-const getUserData=(data)=>{
-  setuserData(data);
-}
+  const getUserData = (data) => {
+    setuserData(data);
+  };
 
   useEffect(() => {
-    var pusher = new Pusher('7efb4ec01e91425deb2f', {
-      cluster: 'ap2'
+    var pusher = new Pusher(`${process.env.REACT_APP_Check_Pusher}`, {
+      cluster: "ap2",
     });
 
-    var channel = pusher.subscribe('masseges');
-    channel.bind('inserted', function(data) {
-      setmessages([...messages,data])
+    var channel = pusher.subscribe("masseges");
+    channel.bind("inserted", function (data) {
+      setmessages([...messages, data]);
     });
-    var channel1 = pusher.subscribe('rooms');
-    channel1.bind('inserted', function(da) {
-      setmyroomData([...myroomData,da])
-      setfetchroomd(true)
+    var channel1 = pusher.subscribe("rooms");
+    channel1.bind("inserted", function (da) {
+      setmyroomData([...myroomData, da]);
+      setfetchroomd(true);
     });
-    return()=>{
-channel.unbind_all();
-channel.unsubscribe();
-channel1.unbind_all();
-channel1.unsubscribe();
-    }
-  }, [messages])
-  const fetchdata=async()=>{
-    await axios.get(`${fetchmessage}`).then((resp)=>setmessages(resp.data)).catch(e=>console.log('error'))
-  }
-  const fetchroomData=async()=>{
-    await axios.get(`${fetchroom}`).then((resp)=>setmyroomData(resp.data)).catch(e=>console.log('error'))
-  }
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+      channel1.unbind_all();
+      channel1.unsubscribe();
+    };
+  }, [messages]);
+  const fetchdata = async () => {
+    await axios
+      .get(`${fetchmessage}`)
+      .then((resp) => setmessages(resp.data))
+      .catch((e) => console.log("error"));
+  };
+  const fetchroomData = async () => {
+    await axios
+      .get(`${fetchroom}`)
+      .then((resp) => setmyroomData(resp.data))
+      .catch((e) => console.log("error"));
+  };
   useEffect(() => {
-    fetchroomData()
-fetchdata()
+    fetchroomData();
+    fetchdata();
   }, []);
   useEffect(() => {
-    fetchroomData()
+    fetchroomData();
   }, [fetchroomd]);
-  return (<>
-  <Routes messages={messages} userData={userData} getUserData={getUserData} myroomData={myroomData}/>
-    
-    
-  </>);
+  return (
+    <>
+      <Routes
+        messages={messages}
+        userData={userData}
+        getUserData={getUserData}
+        myroomData={myroomData}
+      />
+    </>
+  );
 }
 
 export default App;
